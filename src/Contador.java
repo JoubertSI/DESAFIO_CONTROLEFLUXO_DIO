ed6ded4daa798ad1a9cf564c44c6d27122f6bf6a
Joubert <ti.formaestetica@gmail.com>
2024-07-25 17:18:01 -0300

    Iniciando o desafio

---
+++ src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Contador {
+
+  public static void main(String[] args)throws ParametrosInvalidosException {
+  Scanner terminal = new Scanner(System.in);
+
+  System.out.println("Entre com parametroUm:");
+  int parametroUm = terminal.nextInt();
+
+  System.out.println("Entre com o paremetroDois:");
+  int parametroDois = terminal.nextInt();
+
+  try {
+    //chamando o método contendo a lógica de contagem
+    contar(parametroUm, parametroDois);
+  
+  }catch (ParametrosInvalidosException exception) {
+    //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+  }
+  
+}
+static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+  //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+  
+  int contagem = parametroDois - parametroUm;
+  //realizar o for para imprimir os números com base na variável contagem
+}
+}
