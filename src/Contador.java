916dc7a853d4c944d0b9eee2f7b2fa255e57f486
Joubert <ti.formaestetica@gmail.com>
2024-07-31 16:41:03 -0300

    Atualização DesafioControleFluxo

--- src/Contador.java
+++ src/Contador.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Contador {
 
-  public static void main(String[] args)throws ParametrosInvalidosException {
+  public static void main(String[] args){
   Scanner terminal = new Scanner(System.in);
-
   System.out.println("Entre com parametroUm:");
   int parametroUm = terminal.nextInt();
 
@@ -17,13 +16,21 @@ public class Contador {
   
   }catch (ParametrosInvalidosException exception) {
     //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+    System.out.println("O segundo parâmetro deve ser maior do que o primeiro!");
   }
-  
 }
-static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-  //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-  
-  int contagem = parametroDois - parametroUm;
-  //realizar o for para imprimir os números com base na variável contagem
-}
+  static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+    //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+    if (parametroUm >= parametroDois){
+      throw new ParametrosInvalidosException();
+    }
+    
+    int contagem = parametroDois - parametroUm;
+    //realizar o for para imprimir os números com base na variável contagem
+    for (int i = 1; i <= contagem; i++){
+      System.out.println("Imprimindo o número " +i);
+    }
+    
+  }
+
 }
